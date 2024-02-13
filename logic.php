c50f326643ed19e9281b31ef4dd4d8ae3f0df8c9
Niko Hoogeveen <niko.hoogeveen@gmail.com>
2024-02-13 18:18:31 -0500

    Added comments to logic.php

--- logic.php
+++ logic.php
@@ -1,15 +1,17 @@
 <?php
 
+// loop through numbers 1 through 100 inclusive
 for ($x = 1; $x <= 100; $x++ ) {
+    // first we must check case that $x is divisble by both 3 and 5
     if ($x % 3 == 0 && $x % 5 == 0) {
         echo "foobar";
-    } elseif ($x % 3 == 0){
+    } elseif ($x % 3 == 0){ // can check individual cases after
         echo "foo";
     } elseif ($x % 5 == 0) {
         echo "bar";
-    } else {
+    } else {        // default, print the current number if not divisible by 3 or 5
         echo $x;
     }
-    echo ", ";
+    echo ", ";  // print comma and space for ease of viewing
 
 }

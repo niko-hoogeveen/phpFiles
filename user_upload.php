6a076c0f70bdb19e939c9cd177856da7b952f4a9
Niko Hoogeveen <niko.hoogeveen@gmail.com>
2024-02-13 19:03:30 -0500

    Fixed help output (new lines weren't cooperating, added a database connection

--- user_upload.php
+++ user_upload.php
@@ -9,25 +9,44 @@ $options = getopt("f:u:p:h:", ["file:", "create_table", "dry_run", "help"]);
 
 // now must check if help option was supplied
 if (isset($options['help']) || empty($options)) {
-    echo 'Usage: php user_upload.php [options]\n';
-    echo 'Options: \n';
-    echo '--file [csv file name] : Name of CSV to be parsed\n';
-    echo '--create table : build MySQL users table and exit';
-    echo '--dry_run : Run script without executing users into the database';
-    echo '-u : configure MySQL username';
-    echo '-p : configure MySQL password';
-    echo '-h : configure MySQL host';
+    echo "Usage: php user_upload.php [options]\n";
+    echo "Options: \n";
+    echo "--file [csv file name] : Name of CSV to be parsed\n";
+    echo "--create table : build MySQL users table and exit\n";
+    echo "--dry_run : Run script without executing users into the database\n";
+    echo "-u : configure MySQL username\n";
+    echo "-p : configure MySQL password\n";
+    echo "-h : configure MySQL host\n";
     exit(0);
 }
 
 // now we must check if the MySQL username, password and host were given 
 if (!isset($options['u']) || !isset($options['p']) || !isset($options['h'])) {
-    echo 'Error: MySQL username (-u), password (-p), and host (-h) must be provided.\n';
+    echo "Error: MySQL username (-u), password (-p), and host (-h) must be provided.\n";
     exit(1);
 }
 
 // 2. create our DB connection - ensure to add error handling (try & catch on connection attempt)
+
+// Get database connection details
+$servername = $options['h'];
+$username = $options['u'];
+$password = $options['p'];
+$dbname = "database";       // assume database name is static and called 'database'
+
+// Create a database connection - ensure error handling
+$conn = new mysqli($servername, $username, $password) or die("Error: ". mysqli_error($conn));
+
+if ($conn->connect_error) {
+    die("Connection failed: ". mysqli_connect_error());
+}
+
+
 // 3. check for what command line directive was inputted by the user - execution decided by what directive(s) used
+
+// lets do 
+
+
 // Directives: 
 // create_table: create users table in MySQL - table needs to contain name, surname and email address (unique index)
 // dry_run: run the script but don't INSERT into the db - all other functions executed
